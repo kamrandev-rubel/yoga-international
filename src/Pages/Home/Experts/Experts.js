f771360c39f7c8fc90157b8c22619e34999428be
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-04-19 04:42:33 +0300

    complete experts component

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import { Pagination, Navigation, Autoplay, FreeMode } from "swiper";
+import { FreeMode, Pagination, Autoplay } from "swiper";
 import useServiceData from '../../../Hooks/useServiceData';
 
 const Experts = () => {
@@ -13,50 +15,54 @@ const Experts = () => {
       },[services])
 
     return (
-      <div>
+      <div className='my-16'>
         <h2 className="text-[#112734] text-[5vw]">
           Practice with <span className="text-[#375B26]">confidence</span>
         </h2>
-        <p>
+        <p className=' mb-16'>
           Connect and study with 500+ expert teachers across yoga therapy,
           anatomy. meditation, and more. The world’s most diverse yoga site.
         </p>
         <div>
           <Swiper
-            slidesPerView={3}
-            spaceBetween={30}
-            // breakpoints={{
-            //     640: {
-            //       slidesPerView: 2,
-            //       spaceBetween: 20,
-            //     },
-            //     768: {
-            //       slidesPerView: 4,
-            //       spaceBetween: 40,
-            //     },
-            //     1024: {
-            //       slidesPerView: 5,
-            //       spaceBetween: 50,
-            //     },
-            //   }}
-            // autoplay={{
-            //     delay: 2500,
-            //     disableOnInteraction: false,
-            //   }}
+            // slidesPerView={1}
+            spaceBetween={0}
+            loop={true}
+            breakpoints={{
+                300: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                //   spaceBetween: 50,
+                },
+              }}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
             pagination={{
               clickable: true,
             }}
-            modules={[FreeMode, Pagination]}
+            modules={[FreeMode, Autoplay]}
             className="mySwiper"
           >
               {
                   experts.map(expert => {
                       const {img} = expert;
-                    //   console.log(expert);
                       return (
                           
                               <SwiperSlide key={expert.id}> 
-                                <img src={img} alt="" className='w-5/12' />
+                                <img src={img} alt="" className='w-[237px] h-[237px] rounded-full mx-auto' />
                               </SwiperSlide>
                           
                       )
