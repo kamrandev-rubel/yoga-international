9b883260b5c34566a69bf36258586789c5eb5275
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-04-20 00:26:30 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './Pages/Checkout/Checkout';
 import Courses from './Pages/Home/Courses/Courses';
 import Home from './Pages/Home/Home/Home';
 import RequireAuth from './Pages/RequireAuth/RequireAuth';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import SignIn from './Pages/SignIn/SignIn/SignIn';
 import SignUp from './Pages/SignIn/SignUp/SignUp';
@@ -25,6 +26,7 @@ function App() {
         <Route path='/signin' element={<SignIn/>} />
         <Route path='/signup' element={<SignUp/>} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,1 +1,54 @@
-rsc
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {AiFillInstagram} from 'react-icons/ai';
+import {FaTwitter,FaPinterestP,FaFacebookF} from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <div className='bg-black py-[70px] mt-14'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mx-auto'>
+                    <div className='flex  justify-center text-lg'>
+                        <div className='w-64  flex flex-col text-center md:text-left text-gray-100'>
+                            <Link className='mb-2 text-[#D3B10F] font-[700]' to='/'>Courses</Link>
+                            <Link className='mb-2' to='/'>Featured</Link>
+                            <Link className='mb-2' to='/'>Asana</Link>
+                            <Link className='mb-2' to='/'>Ayurveda</Link>
+                            <Link className='mb-2' to='/'>Conferance</Link>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center text-lg'>
+                        <div className='w-64  flex flex-col text-center md:text-left text-gray-100'>
+                            <Link className='mb-2 text-[#D3B10F] font-[700]' to='/'>Classes</Link>
+                            <Link className='mb-2' to='/'>Beginner</Link>
+                            <Link className='mb-2' to='/'>Hatha Yoga</Link>
+                            <Link className='mb-2' to='/'>Prenatial</Link>
+                            <Link className='mb-2' to='/'>Restorative</Link>
+                            <Link className='mb-2' to='/'>Vinyasa</Link>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center text-gray-100 text-lg'>
+                        <div className='w-64  flex flex-col text-center md:text-left '>
+                            <Link className='mb-2 text-[#D3B10F] font-[700]' to='/'>About</Link>
+                            <Link className='mb-2 text-gray-400' to='/'>Need help&</Link>
+                            <Link className='mb-2 text-gray-400' to='/'>Careers</Link>
+                            <Link className='mb-2 text-gray-400' to='/'>Privacy Policy</Link>
+                            <Link className='mb-2 text-gray-400' to='/'>Terms of Service</Link>
+                        </div>
+                    </div>
+            </div>
+            <div className='w-[90%] mx-auto flex justify-between flex-col md:flex-row'>
+                <div className='mt-[100px]'>
+                    <p className='text-gray-400'><small>Copyright &copy; {new Date().getFullYear()} online food</small></p>
+                </div>
+                <div className='text-gray-100 text-lg flex mt-[80px] justify-center'>
+                    <AiFillInstagram className='bg-[#F1D44B] w-9 h-9 ml-8 p-1.5 rounded-full text-gray-900 '/>
+                    <FaTwitter className='bg-[#F1D44B] w-9 h-9 ml-8 p-1.5 rounded-full text-gray-900 '/>
+                    <FaPinterestP className='bg-[#F1D44B] w-9 h-9 ml-8 p-1.5 rounded-full text-gray-900 '/>
+                    <FaFacebookF className='bg-[#F1D44B] w-9 h-9 ml-8 p-1.5 rounded-full text-gray-900 '/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
